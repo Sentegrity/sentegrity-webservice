84f8b8d9a81766081f5a114385652c047c4edd48
Ante Braovic <abraovic@gmail.com>
2016-07-10 15:17:58 +0200

    admin group tests added

--- src/symfony/src/Sentegrity/BusinessBundle/Tests/Services/Admin/GroupTest.php
+++ src/symfony/src/Sentegrity/BusinessBundle/Tests/Services/Admin/GroupTest.php
@@ -22,10 +22,14 @@ class GroupTest extends WebTestCase
 
         // first we need to create two new policies, both for iOS and Android
         Utility::init(static::createClient()->getContainer());
+        Utility::setUserSession();
         self::$iosUuid = Utility::createPolicy(Platform::IOS);
         self::$androidUuid = Utility::createPolicy(Platform::ANDROID);
     }
-    
+
+    /**
+     * @group admin_group
+     */
     public function testGetGroupByGroupAndOrganization()
     {
         /** @var Organization $adminOrganizationService */
@@ -40,7 +44,9 @@ class GroupTest extends WebTestCase
             "contact_email" => 'contact.email@domain.test',
             "contact_phone" => '+1 234 5678',
             "policy_ios" => self::$iosUuid,
-            "policy_android" => self::$androidUuid
+            "policy_android" => self::$androidUuid,
+            "username" => "admin",
+            "password" => "pass"
         );
 
         $uuid = $adminOrganizationService->create($organizationData);
@@ -49,5 +55,67 @@ class GroupTest extends WebTestCase
         $rsp = self::$groupService->getGroupByGroupAndOrganization(0, $org);
         $this->assertInstanceOf('\Sentegrity\BusinessBundle\Entity\Documents\Groups', $rsp);
         $this->assertEquals($rsp->getOrganization()->getUuid(), $uuid->data);
+    }
+
+    /**
+     * @group admin_group
+     */
+    public function testDeleteGroupsByOrganization()
+    {
+        /** @var Organization $adminOrganizationService */
+        $adminOrganizationService = static::createClient()
+            ->getContainer()
+            ->get('sentegrity_business.organization');
+
+        $organizationData = array(
+            "name" => 'Test organization name',
+            "domain_name" => 'domain.test',
+            "contact_name" => 'Contact Name',
+            "contact_email" => 'contact.email@domain.test',
+            "contact_phone" => '+1 234 5678',
+            "policy_ios" => self::$iosUuid,
+            "policy_android" => self::$androidUuid,
+            "username" => "admin",
+            "password" => "pass"
+        );
+
+        $uuid = $adminOrganizationService->create($organizationData);
+        $org = $adminOrganizationService->getOrganizationByUuid($uuid->data);
+
+        $rsp = self::$groupService->deleteGroupsByOrganization($org);
+        $this->assertEquals($rsp, 1, "Delete failed");
+    }
+
+    /**
+     * @group admin_group
+     */
+    public function testGetDefaultGroupsByMultipleOrganizations()
+    {
+        /** @var Organization $adminOrganizationService */
+        $adminOrganizationService = static::createClient()
+            ->getContainer()
+            ->get('sentegrity_business.organization');
+
+        $organizationData = array(
+            "name" => 'Test organization name',
+            "domain_name" => 'domain.test',
+            "contact_name" => 'Contact Name',
+            "contact_email" => 'contact.email@domain.test',
+            "contact_phone" => '+1 234 5678',
+            "policy_ios" => self::$iosUuid,
+            "policy_android" => self::$androidUuid,
+            "username" => "admin",
+            "password" => "pass"
+        );
+
+        $uuid = $adminOrganizationService->create($organizationData);
+        $org[] = $adminOrganizationService->getOrganizationByUuid($uuid->data);
+        $uuid = $adminOrganizationService->create($organizationData);
+        $org[] = $adminOrganizationService->getOrganizationByUuid($uuid->data);
+        $uuid = $adminOrganizationService->create($organizationData);
+        $org[] = $adminOrganizationService->getOrganizationByUuid($uuid->data);
+
+        $rsp = self::$groupService->getDefaultGroupsByMultipleOrganizations($org);
+        $this->assertCount(3, $rsp, "Wrong number of groups returned");
     }
 }
